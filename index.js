6b59a42945a027952e72db4c653ee95f3aeb13fc
fiona12-code <fionaomondi77@gmail.com>
2025-05-11 12:13:51 +0300

    User experience

--- index.js
+++ index.js
@@ -16,8 +16,12 @@ function generateRecipe(event) {
   let apiKey = "3553b3fea60f2bt4edfefb3ad730a38o";
   let prompt = `generate a recipe about ${instructionsInput.value}`;
   let context =
-    "You are a world-renowned chef who loves writing recipes from around the world, your mission is to generate simple, short and easy to follow recipes. Write in html format but do not display that you are writing in html format";
+    "You are a world-renowned chef who loves writing recipes from around the world, your mission is to generate simple, short and easy to follow recipes. Write in html format but do not display that you are writing in html format. Do not display the heading element";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let recipeElement = document.querySelector("#recipe");
+  recipeElement.classList.remove("hidden");
+  recipeElement.innerHTML = `<div class="generating">⏳ Generating recipe about ${instructionsInput.value}...</div>`;
 
   console.log("Generating recipe");
   console.log(`prompt: ${prompt}`);
